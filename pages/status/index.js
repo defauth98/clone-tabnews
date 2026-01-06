267e78389957a31c8a75d8d3c848b669660fa11d
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2026-01-06 11:07:23 -0300

    feat: add status page in `/status`

---
+++ pages/status/index.js
@@ -0,0 +1,39 @@
+import useSWR from "swr";
+
+async function fetchStatus() {
+  const response = await fetch("/api/v1/status");
+  const responseBody = await response.json();
+  return responseBody;
+}
+
+export default function StatusPage() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchStatus, {
+    refreshInterval: 2000,
+  });
+
+  let date = "Carrengando...";
+
+  if (!isLoading && data) {
+    date = new Date(data.updated_at).toLocaleString("pt-BR");
+  }
+
+  return (
+    <>
+      <div>
+        Data atual: <strong>{date}</strong>
+      </div>
+      <div>
+        Versão do Postgres:{" "}
+        <strong>{data.dependencies.database.version}</strong>
+      </div>
+      <div>
+        Conexões máximas:{" "}
+        <strong>{data.dependencies.database.maximum_connections}</strong>
+      </div>
+      <div>
+        Conexões abertas:{" "}
+        <strong>{data.dependencies.database.opened_connections}</strong>
+      </div>
+    </>
+  );
+}
