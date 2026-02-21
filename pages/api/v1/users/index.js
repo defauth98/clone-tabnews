ea597c96f66bfbd0f91d8409409ced83aba8844b
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2026-02-21 11:59:41 -0300

    feat: send activation email after user registration

--- pages/api/v1/users/index.js
+++ pages/api/v1/users/index.js
@@ -1,6 +1,7 @@
 import { createRouter } from "next-connect";
 import controller from "infra/controller";
 import user from "models/user";
+import activation from "models/activation";
 
 const router = createRouter();
 router.post(postHandler);
@@ -9,5 +10,9 @@ export default router.handler(controller.errorHandlers);
 async function postHandler(request, response) {
   const userInputValues = request.body;
   const newUser = await user.create(userInputValues);
+
+  const activationToken = await activation.create(newUser.id);
+  await activation.sendEmailToUser(newUser, activationToken);
+
   response.status(201).json(newUser);
 }
