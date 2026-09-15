423661940e6e8b5b577eb06f22113248bc239b2a
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2026-09-14 23:11:05 -0300

    feat: add `contentWidth` support to `DefaultLayout`

--- interface/DefaultLayout/index.js
+++ interface/DefaultLayout/index.js
@@ -1,7 +1,18 @@
 import { Header, PageLayout, Text } from "@primer/react";
 import Head from "next/head";
+import styles from "./index.module.css";
 
-export default function DefaultLayout({ children, metadata = {} }) {
+const contentWidthClasses = {
+  small: styles.smallContent,
+};
+
+export default function DefaultLayout({
+  children,
+  metadata = {},
+  contentWidth,
+}) {
+  const extraContentClassName = contentWidthClasses[contentWidth];
+
   return (
     <>
       <Head>
@@ -28,7 +39,12 @@ export default function DefaultLayout({ children, metadata = {} }) {
 
       <PageLayout>
         <PageLayout.Header></PageLayout.Header>
-        <PageLayout.Content>{children}</PageLayout.Content>
+        <PageLayout.Content
+          width={contentWidth}
+          className={extraContentClassName}
+        >
+          {children}
+        </PageLayout.Content>
         <PageLayout.Footer divider="line">
           <Text size="small">© {new Date().getFullYear()}</Text>
         </PageLayout.Footer>
